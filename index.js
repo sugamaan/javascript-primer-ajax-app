c9a1df922526082f4369f20535aa78df41ab6739
sugama <reiji.sugama@gmail.com>
2024-07-18 13:49:52 +0900

    HTTP通信まで実装

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("index.js: loaded");
+
+function getUserInfo(userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((response) => {
+      console.log(response.ok);
+      return response.json().then((userInfo) => {
+        console.log(userInfo);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
